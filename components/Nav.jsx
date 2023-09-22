dc576182f826d55c6a5a42cf3bbd939cf163a84e
roms <rusirusamaraweera749@gmail.com>
2023-09-22 18:15:34 +0530

    modify nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,18 +8,18 @@ import '../styles/nav.css'
 export default function Nav() {
   return (
 
-    <div className="flex flex-row px-14 nav">
-        <div className="logo basis-1/12 sm:basis-2/12">
-            <Image className='image' src={logo} alt='logo image' />
+    <div className="flex flex-row lg:px-14 sm:px-6 nav">
+        <div className="logo relative w-32 h-52">
+            <Image className='image object-cover' src={logo} alt='logo image' fill />
         </div>
-        <div className="flex flex-col basis-11/12 pt-14">
-            <h1 className='flex justify-center text-xl text-center  headingText'>L&apos;OPERĀ INTERNATIONAL SCHOOL</h1>
-            <div className="space-x-48 flex justify-center  mt-8 link">
+        <div className="flex flex-col w-full pt-12">
+            <h1 className='flex justify-center text-center  headingText md:text-center sm:text-[16px] text-[27px]'>L&apos;OPERĀ INTERNATIONAL SCHOOL</h1>
+            <div className="flex justify-around mt-5 uppercase link">
                  <Link href={'/home'}>Home</Link>
                  <Link href={'/aboutus'}>About Us</Link>
                  <Link href={'/courses'}>Courses</Link>
-                 <Link href={'/contact'}>Contact</Link>
                  <Link href={'/gallery'}>Gallery</Link>
+                 <Link href={'/contact'}>Contact</Link>
             </div>
         </div>
     </div>
