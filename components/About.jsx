4a517fb3a4595989e2d5566bb47728b28558ff63
roms <rusirusamaraweera749@gmail.com>
2023-09-28 19:21:27 +0530

    styling homepage about section

--- components/About.jsx
+++ components/About.jsx
@@ -1,17 +1,53 @@
-import Image from 'next/image'
-import React from 'react'
-import homepageAboutSection from '../public/homepageAboutSection.png'
+import Image from 'next/image';
+import React from 'react';
+import homepageAboutSection from '../public/homepageAboutSection.png';
+import { AiOutlineCaretUp } from 'react-icons/ai';
+import Button from './Button';
 
 export default function About() {
   return (
-    <div className='flex flex-row gap-0 md:py-14 py-4 homepageAboutSectionDiv'>
-        <div className='hidden md:block md:w-1/2 w-full relative h-screen'>
-          <Image src={homepageAboutSection} alt='a girl holding a headphone' className='object-cover absolute  h-[100%] w-[100%] left-[-56px] right-0 top-0 bottom-0 homepageAboutSectionImage'/>
-        </div>
-        <div className='md:w-1/2 md:pt-20 sm:px-11 pt-6 flex flex-col'>
-          <p className='uppercase text-[#523735] lg:text-[1rem] lg:heading-[1.5rem] md:text-[0.875rem] md:leading-[1.25rem] font-[roboto]'>About us</p>
-          <h2 className=''>PROFESSIONALITÀ ED ESPERIENZA</h2>
-        </div>
+    <div className='flex flex-row gap-0 md:py-14 py-4 '>
+      <div className='hidden md:block md:w-1/2 w-full relative h-screen'>
+        <Image
+          src={homepageAboutSection}
+          alt='a girl holding a headphone'
+          className='object-cover absolute  h-[100%] w-[100%] left-[-56px] right-0 top-0 bottom-0 '
+          priority
+        />
+      </div>
+      <div className='md:w-1/2 md:pt-20 sm:px-11 md:px-0 pt-6 flex flex-col'>
+        <p className='uppercase text-[#c29e75] lg:text-[1rem] lg:heading-[1.5rem] md:text-[0.875rem] md:leading-[1.25rem] font-[roboto]'>
+          About us
+        </p>
+        <h2 className='lg:leading-[4rem] lg:text-5xl md:leading-[2.25rem] md:text-3xl md:my-0 uppercase font-semibold text-4xl font-[roboto] .mt-1 mb-2'>
+          PROFESSIONALITÀ ED ESPERIENZA
+        </h2>
+        <p className='text-sm font-[roboto] my-2 '>
+          La tua scuola di musica a Milano
+        </p>
+        <p className='text-base font-[roboto] my-3 '>
+          L’Opera International Music School è fondata sulla passione per la
+          musica e l’ambizione araggiungere un’educazione musicale superiore.I
+          nostri docenti sono selezionati non solo per la loro formazione
+          accademica, ma anche per il loro solido background nel campo della
+          musica.
+        </p>
+        <ul className='flex flex-col text-[#d9d9d9] text-sm gap-2'>
+          <li>
+            <AiOutlineCaretUp className='inline rotate-90 ml-[-5px] text-[#c29e75] ' />
+            Professionalità
+          </li>
+          <li>
+            <AiOutlineCaretUp className='inline rotate-90 ml-[-5px] text-[#c29e75] ' />
+            Metodo
+          </li>
+          <li>
+            <AiOutlineCaretUp className='inline rotate-90 ml-[-5px] text-[#c29e75] ' />
+            Qualità
+          </li>
+        </ul>
+        <Button text={'Saperne di più'} textColor={'#C29E75'} bgColor={'#523735'} border={undefined} path={'/about'}  />
+      </div>
     </div>
-  )
+  );
 }
--- components/Button.jsx
+++ components/Button.jsx
@@ -1,22 +1,25 @@
+'use client'
+
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 
 export default function Button({
   text,
-  border,
-  bgColor,
+  border = undefined,
+  bgColor = 'transparent',
   textColor = '#fff',
-  hoverBgColor,
+  hoverBgColor = undefined,
   path,
 }) {
+  console.log(text, border, bgColor, textColor, hoverBgColor, path);
 
   const router = useRouter();
 
   return (
     <button
       className={`group rounded-[5px] ${border} bg-[${bgColor}] py-[0.5rem] px-[1.5rem] lg:text-[1.2rem] tracking-[0.02rem] lg:leading-[1.75rem] md:text-[1rem] md:leading-[1.5rem] h-12 text-[${textColor}] hover:bg-[${hoverBgColor}] transition-all mr-4 hover:scale-[1.02]`}
-      onClick={()=> router.push(`/${path}`)}
+      onClick={() => router.push(`/${path}`)}
     >
       {text}
       <span className='pl-4 font-bold inline-block align-middle group-hover:animate-[pulse_1s_ease-in-out_infinite] '>
