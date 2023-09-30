b9ea98242d3f2b09577fb9c6579a73b6ec764512
roms <rusirusamaraweera749@gmail.com>
2023-09-30 23:46:19 +0530

    making home page our course section

--- components/OurCourseImageSlide.jsx
+++ components/OurCourseImageSlide.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import bassGuitar1 from '../public/bass-guitar1.png';
+import Image from 'next/image';
 
 export default function OurCourseImageSlide() {
+  const images = [
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+    { image: bassGuitar1, course: 'Bass guitar' },
+  ];
+
   return (
     <div>
-        {/* Image Slide */}
+        <div className='flex flex-row  '>
+        {images.map((card, index) => (
+            <div key={index} className='flex flex-col'>
+            <div>
+                <Image alt='{card.course}' src={card.image} />
+            </div>
+            <div className='py-4'>
+                <p>{card.course}</p>
+            </div>
+            </div>
+        ))}
+        </div>
     </div>
-  )
+  );
 }
