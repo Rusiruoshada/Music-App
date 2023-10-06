528bbada519833bd094919b608f9216e9be1790d
roms <rusirusamaraweera749@gmail.com>
2023-10-06 13:43:21 +0530

    adding down footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UpperFooter from './UpperFooter'
+import DownFooter from './DownFooter'
 
 export default function Footer() {
   return (
     <>
         <UpperFooter />
+        <DownFooter />
     </>
   )
 }
