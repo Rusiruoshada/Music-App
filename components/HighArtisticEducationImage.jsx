1d38e4682850ff1fc71caa7a9c2c63d0e538d913
roms <rusirusamaraweera749@gmail.com>
2023-10-07 22:17:17 +0530

    finish homepage

--- components/HighArtisticEducationImage.jsx
+++ components/HighArtisticEducationImage.jsx
@@ -6,8 +6,8 @@ import reward2 from '../public/reward2.png';
 export default function HighArtisticEducationImage() {
   return (
     <div className='py-14 space-even w-full flex flex-row padding'>
-      <div className='flex flex-col sm:flex-row w-full lg:w-1/2 justify-center '>
-        <div className='flex flex-col relative rounded-[0.635rem] aspect-square bg-gradient-to-t from-[#c29e75] w-64'>
+      <div className='flex flex-col sm:flex-row w-full lg:w-1/2 justify-center'>
+        <div className='flex flex-col relative rounded-[0.635rem] aspect-square bg-gradient-to-t from-[#c29e75] w-64 hover:scale-105 transition ease-in-out duration-200 hover:shadow-2xl hover:shadow-cyan-900'>
           <div className='pl-16 flex-col flex my-auto'>
             <span className='text-white font-bold text-lg items-center flex mt-5 mr-4 '>
               Trinity College London - Digital Transformation Award
@@ -26,10 +26,10 @@ export default function HighArtisticEducationImage() {
         </div>
       </div>
       <div className='flex flex-row justify-center w-full lg:w-1/2 '>
-        <div className='flex flex-col relative rounded-[0.635rem] aspect-square bg-gradient-to-t from-[#c29e75] w-64'>
+        <div className='flex flex-col relative rounded-[0.635rem] aspect-square bg-gradient-to-t from-[#c29e75] w-64 hover:scale-105 transition ease-in-out duration-200 hover:shadow-2xl hover:shadow-cyan-900'>
           <div className='pl-16 flex-col flex my-auto'>
             <span className='text-white font-bold text-lg items-center flex mt-5 mr-4 '>
-              Trinity College London - Digital Transformation Award
+              Trinity College London - Digital Transformation Teacher
             </span>
           </div>
           <div className='absolute left-[-40%] sm:left-[-120px] md:left-[-140px]  lg:left-[-140px]'>
